c448a617f6c03cafde750d871e5d1a10c0575308
masterUNG <phrombutr@gmail.com>
2019-02-23 11:54:53 +0700

    Initial commit

---
+++ app/src/main/java/masterung/th/in/androidthai/ungfriend/MainActivity.java
@@ -0,0 +1,23 @@
+package masterung.th.in.androidthai.ungfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+
+
+
+    }   // Main Method
+
+}   // Main Class
