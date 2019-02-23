1a93d455e19bf99f1737cd0496fcc9cdf38b25b0
masterUNG <phrombutr@gmail.com>
2019-02-23 14:44:28 +0700

    Arrow Navigator Success

--- app/src/main/java/masterung/th/in/androidthai/ungfriend/MainFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/ungfriend/MainFragment.java
@@ -2,10 +2,12 @@ package masterung.th.in.androidthai.ungfriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -20,10 +22,29 @@ public class MainFragment extends Fragment {
 
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment, new RegisterFragment()).addToBackStack(null).commit();
+
+            }
+        });
+
+
+    }   // Main Method
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-}
+}   // Main Class
